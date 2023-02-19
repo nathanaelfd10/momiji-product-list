a7f75c71d05ed2bfd9a5b4abc34de6104399b14f
Fernando N <64892153+nathanaelfd10@users.noreply.github.com>
2023-02-19 23:05:17 +0700

    Change queue classes, renamed duplicate class JobHandler -> QueueHandler

--- src/main/java/com/noxfl/momijitreehouse/amqp/QueueHandler.java
+++ src/main/java/com/noxfl/momijitreehouse/amqp/QueueHandler.java
@@ -10,13 +10,13 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
  *
  */
 
-public class JobHandler {
-	
-	@RabbitListener(queues = "treehouse")
+public class QueueHandler {
+
+	@RabbitListener(queues = "${queue.name.read}")
 	public void jobReceiver(String in) throws InterruptedException {
 		receive(in);
 	}
-	
+
 	private void receive(String in) {
 		System.out.println("Job received: " + in);
 	}
