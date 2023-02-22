21420d6c6fcc246531c2d5f5ca78532f8bf09efb
Fernando N <64892153+nathanaelfd10@users.noreply.github.com>
2023-02-22 23:37:15 +0700

    Add function to StringUtils

--- src/main/java/com/noxfl/momijitreehouse/util/StringUtils.java
+++ src/main/java/com/noxfl/momijitreehouse/util/StringUtils.java
@@ -3,6 +3,10 @@
  */
 package com.noxfl.momijitreehouse.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * @author Fernando Nathanael
  *
@@ -32,5 +36,56 @@ public class StringUtils {
 		String defaultSeparator = "-";
 		return slugify(string, defaultSeparator, defaultSplitter);
 	}
+	
+	/**
+	 * Generates a random letter
+	 * 
+	 * @return random letter
+	 */
+    public static char generateRandomLetter() {
+        Random random = new Random();
+        char c = (char)(random.nextInt(26) + 'a');
+        return c;
+    }
+    
+    /**
+     * Generates a string of random letters
+     * 
+     * @param length
+     * @return a stringn of random letters
+     */
+    public static String generateRandomLetters(int length) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i < length; i++) {
+            char randomLetter = generateRandomLetter();
+            sb.append(randomLetter);
+        }
+        
+        String output = sb.toString();
+        
+        return output;
+        
+    }
+    
+    /**
+     * Generates strings of 3 random letters
+     * 
+     * @param count
+     * @return strings of 3 random letters (example: abc-def-ghi)
+     */
+    public static String generateRandom3Letters(int count) {
+        
+        List<String> stringOfRandomLetters = new ArrayList<>();
+        
+        for(int i=0; i< count; i++) {
+            stringOfRandomLetters.add(generateRandomLetters(3));
+        }
+        
+        String output = String.join("-", stringOfRandomLetters);
+        
+        return output;
+    }
 
 }
